65f0486e5b6bf2bb80f27b195653654436874af8
Tomas Hozza <thozza@gmail.com>
2018-12-26 19:18:19 +0100

    Updated to MySensors v2.3.0
    - added HTU21 sensor support
    
    - refactored code to allow selection of connected sensors
    
    Signed-off-by: Tomas Hozza <thozza@gmail.com>

--- README.md
+++ README.md
@@ -6,42 +6,51 @@ about the requirements, please read on.
 
 ## Hardware Prerequisites
 
-The shetch is intended for Arduino compatible board [Devduino v2.0](http://www.seeedstudio.com/wiki/DevDuino_Sensor_Node_V2.0_(ATmega_328)). The board comes with MCP9700 chip, which is an analog temperature sensor. This sketch also uses digital temperature and humidity sensor [DHT22](https://www.sparkfun.com/datasheets/Sensors/Temperature/DHT22.pdf) for maximum precision. The sensor node is wireless, and uses the [nRF24L01+ wireless module](https://www.sparkfun.com/datasheets/Wireless/Nordic/nRF24L01P_Product_Specification_1_0.pdf) for communication with sensor gateway.
+The shetch is intended for Arduino compatible board [Devduino v2.0](http://www.seeedstudio.com/wiki/DevDuino_Sensor_Node_V2.0_(ATmega_328)). The board comes with MCP9700 chip, which is an analog temperature sensor. This sketch also uses digital temperature and humidity sensor [DHT22](https://www.sparkfun.com/datasheets/Sensors/Temperature/DHT22.pdf) and/or sensor [HTU21/SHU21](https://www.adafruit.com/product/1899)for maximum precision. The sensor node is wireless, and uses the [nRF24L01+ wireless module](https://www.sparkfun.com/datasheets/Wireless/Nordic/nRF24L01P_Product_Specification_1_0.pdf) for communication with sensor gateway.
 
 ### Complete List
 
 * [Devduino v2.0 board](http://www.seeedstudio.com/wiki/DevDuino_Sensor_Node_V2.0_(ATmega_328))
 * [nRF24L01+ wireless module](https://www.sparkfun.com/datasheets/Wireless/Nordic/nRF24L01P_Product_Specification_1_0.pdf)
 * [DHT22 sensor](http://www.seeedstudio.com/depot/Grove-TemperatureHumidity-Sensor-Pro-p-838.html) [(datasheet)](https://www.sparkfun.com/datasheets/Sensors/Temperature/DHT22.pdf)
+* [HTU21/SHU21 sensor](https://www.adafruit.com/product/1899)
 
 ## Software Prerequisites a.k.a. Libraries
 
-For communication with the DHT22 sensor the [arduino-DHT library from markruys](https://github.com/markruys/arduino-DHT) is used. Note that there are many versions of different DHT libraries. If you want the sketch to work out-of-the-box, use the same library. As wireless communication protocol library, the [MySensors library](http://www.mysensors.org/) is used.
-
-### Complete List
-
-* [MySensors library](http://www.mysensors.org/)
-  * For [**master** branch of devduino-temp-hum-sensor](https://github.com/thozza/devduino-temp-hum-sensor/tree/master) use the [**development version** of MySensors library](https://github.com/mysensors/Arduino/tree/development )
-  * If you want to use the [**version 1.4** of MySensors library (latest stable)](https://github.com/mysensors/Arduino/tree/master) use the [**mysensors-protocol-1.4** branch of devduino-temp-hum-sensor](https://github.com/thozza/devduino-temp-hum-sensor/tree/mysensors-protocol-v1.4)
-* [arduino-DHT library from markruys](https://github.com/markruys/arduino-DHT)
+Library dependencies are handled by PlatformIO. For the complete list, please see **platformio.ini** file inside the repository.
 
 ## How it works?
 
-The sketch has the **node ID** hardcoded to **1**. If you want to use different **node ID** or want to automatically request the **node ID**, redefine the ```NODE_ID``` in the sketch. The shetch also uses the default settings of [MySensors library](http://www.mysensors.org/) for the **nRF24L01+** module (like channel, etc.). The sensor node goes to sleep for **5 seconds** after sending all values to the gateway. To change this, redefine the ```SLEEP_TIME``` to different value in **milliseconds**.
+The sketch does not have the **node ID** set. Its assignment is left up to MySensors library. If you want to hardcode **node ID**, uncomment and define the ```MY_NODE_ID``` in the sketch. The shetch also uses the default settings of [MySensors library](http://www.mysensors.org/) for the **nRF24L01+** module (like channel, etc.). The sensor node wakes up from sleep every **5 minutes** after sending all values to the gateway. To change this, redefine the ```SLEEP_TIME``` to different value in **milliseconds**.
 
-### Sensor Setup Phase
+### How to choose connected sensors
+
+To choose connected sensors, just comment out one of the following lines in the sketch:
+
+```
+// Comment out if you don't want to use these sensors
+#define WITH_SENSOR_DHT22     // connected to the DIGITAL connector
+#define WITH_SENSOR_MCP9700   // on board
+#define WITH_SENSOR_HTU21     // connected to the I2C connector
+```
+
+### Sensor Setup and Presentation Phase
 1. Initialization of the sensor node
 2. Send the sketch info to the gateway
 3. Present all connected sensors
   * Sensor ID **0** - DHT22 sensor humidity
   * Sensor ID **1** - DHT22 sensor temperature
   * Sensor ID **2** - MCP9700 sensor temperature
+  * Sensor ID **3** - HTU21 sensor humidity
+  * Sensor ID **4** - HTU21 sensor temperature
 
 ### Sensor Execution Phase
-* Each **5 seconds** do the following:
+* Each **5 minutes** do the following:
 
 1. send battery level in percents
 2. send DHT22 sensor humidity
-3. DHT22 sensor temperature
-4. MCP9700 sensor temperature
-5. go to sleep
+3. send DHT22 sensor temperature
+4. send MCP9700 sensor temperature
+5. send HTU21 sensor humidity
+6. send HTU21 sensor temperature
+7. go to sleep
--- src/devduino-temp-hum-sensor.ino
+++ src/devduino-temp-hum-sensor.ino
@@ -1,7 +1,7 @@
 /*
  * devduino-temp-hum-sensor.ino - Firmware for DevDuino v2.0 based temperature and humidity sensor Node with nRF24L01+ module
  *
- * Copyright 2014-2015 Tomas Hozza <thozza@gmail.com>
+ * Copyright 2014-2018 Tomas Hozza <thozza@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,88 +19,180 @@
  * Tomas Hozza <thozza@gmail.com>
  *
  * MySensors library - http://www.mysensors.org/
- * DHT library - https://github.com/markruys/arduino-DHT
+ * DHT library - https://github.com/adafruit/DHT-sensor-library
+ * SHT21/HTU21 library - https://github.com/adafruit/Adafruit_HTU21DF_Library
  * DevDuino v2.0 - http://www.seeedstudio.com/wiki/DevDuino_Sensor_Node_V2.0_(ATmega_328)
+ *               - https://seeeddoc.github.io/DevDuino_Sensor_Node_V2.2-ATmega_328/
  * nRF24L01+ spec - https://www.sparkfun.com/datasheets/Wireless/Nordic/nRF24L01P_Product_Specification_1_0.pdf
+ *
+ *  * Version:
+ * 0.2
+ *  - port to MySensors 2.3.0
+ *  - added HTU21 sensor support
+ *  - refactored code to allow selection of connected sensors
+ *
  */
 
-#include <MyTransportNRF24.h>
-#include <MyHwATMega328.h>
-#include <MyMessage.h>
-#include <MySensor.h>
+#define MY_DEBUG
+#define MY_DEBUG_VERBOSE
+//#define MY_DEBUG_VERBOSE_RF24
+//#define MY_SIGNAL_REPORT_ENABLED
 
-#include <SPI.h>
+#define MY_RADIO_RF24
+
+#define MY_RF24_CE_PIN 8
+#define MY_RF24_CS_PIN 7
+#define MY_RF24_IRQ_PIN 2
+
+/**
+ * - 0 => 2400 Mhz (RF24 channel 1)
+ * - 1 => 2401 Mhz (RF24 channel 2)
+ * - 76 => 2476 Mhz (RF24 channel 77)
+ * - 83 => 2483 Mhz (RF24 channel 84)
+ * - 124 => 2524 Mhz (RF24 channel 125)
+ * - 125 => 2525 Mhz (RF24 channel 126)
+ * @see http://www.bundesnetzagentur.de/SharedDocs/Downloads/DE/Sachgebiete/Telekommunikation/Unternehmen_Institutionen/Frequenzen/Allgemeinzuteilungen/2013_10_WLAN_2,4GHz_pdf.pdf
+ */
+#define MY_RF24_CHANNEL         76             //RF channel for the sensor net, 0-127
+#define MY_RF24_DATARATE        RF24_250KBPS   //RF24_250KBPS for 250kbs, RF24_1MBPS for 1Mbps, or RF24_2MBPS for 2Mbps
+#define MY_RF24_PA_LEVEL 	      RF24_PA_MAX    //Sensor PA Level == RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBM, and RF24_PA_MAX=0dBm
+#define MY_RF24_BASE_RADIO_ID   0x00,0xFC,0xE1,0xA8,0xA8
+
+//#define MY_RX_MESSAGE_BUFFER_FEATURE
+
+//#define MY_NODE_ID 1
+//#define MY_PARENT_NODE_ID 0
+
+#define MY_DEFAULT_LED_BLINK_PERIOD 300
+
+#define MY_DEFAULT_ERR_LED_PIN 9  // Error led pin - Built in LED on board
+#define MY_DEFAULT_RX_LED_PIN  A6  // Receive led pin - Not connected / used
+#define MY_DEFAULT_TX_LED_PIN  A7  // Transmit led pin - Not connected / used
+
+#define MY_SPLASH_SCREEN_DISABLED
+
+#include <MySensors.h>
+
+/**************************** SKETCH DEFINITIONS ****************************/
+
+// Comment out if you don't want to use these sensors
+#define WITH_SENSOR_DHT22     // connected to the DIGITAL connector
+#define WITH_SENSOR_MCP9700   // on board
+#define WITH_SENSOR_HTU21     // connected to the I2C connector
+
+#ifdef WITH_SENSOR_DHT22
 #include <DHT.h>
+#endif
+
+#ifdef WITH_SENSOR_HTU21
+#include <Wire.h>
+#include <Adafruit_HTU21DF.h>
+#endif
+
 #include <stdint.h>
 
-#define DEBUG 1
-#define SLEEP_TIME 5000
-#define NODE_ID 1
+#define SLEEP_TIME 300000  // 5min
 
+#ifdef WITH_SENSOR_DHT22
 #define SENSOR_DHT22_HUM 0
 #define SENSOR_DHT22_TEMP 1
-#define SENSOR_MCP9700_TEMP 2
 
-/***********************************/
-/********* PIN DEFINITIONS *********/
-/***********************************/
-#define LED_pin 9
-#define DHT22_pin 3
-#define RF24_CE_pin 8
-#define RF24_CS_pin 7
-#define MCP9700_pin A3
+#define DHT_PIN 3
+#define DHT_TYPE DHT22
 
-/*****************************/
-/********* FUNCTIONS *********/
-/*****************************/
-float readMCP9700Temp();
 float readDHT22Hum();
 float readDHT22Temp();
+
+MyMessage msgDHT22Hum(SENSOR_DHT22_HUM, V_HUM);
+MyMessage msgDHT22Temp(SENSOR_DHT22_TEMP, V_TEMP);
+
+DHT dht(DHT_PIN, DHT_TYPE);
+#endif /* WITH_SENSOR_DHT22 */
+
+#ifdef WITH_SENSOR_MCP9700
+#define SENSOR_MCP9700_TEMP 2
+
+#define MCP9700_pin A3
+
+float readMCP9700Temp();
+
+MyMessage msgMCP9700Temp(SENSOR_MCP9700_TEMP, V_TEMP);
+#endif /* WITH_SENSOR_MCP9700 */
+
+#ifdef WITH_SENSOR_HTU21
+#define SENSOR_HTU21_HUM 3
+#define SENSOR_HTU21_TEMP 4
+
+float readHTU21Hum();
+float readHTU21Temp();
+
+MyMessage msgHTU21Hum(SENSOR_HTU21_HUM, V_HUM);
+MyMessage msgHTU21Temp(SENSOR_HTU21_TEMP, V_TEMP);
+
+Adafruit_HTU21DF htu = Adafruit_HTU21DF();
+#endif /* WITH_SENSOR_HTU21 */
+
 uint16_t readVcc();
 uint8_t getVccLevel();
 
-/************************************/
-/********* GLOBAL VARIABLES *********/
-/************************************/
-MyHwATMega328 hw;
-MyTransportNRF24 transport(RF24_CE_pin, RF24_CS_pin);
-
-MySensor node(transport, hw);
-MyMessage msgDHT22Hum(SENSOR_DHT22_HUM, S_HUM);
-MyMessage msgDHT22Temp(SENSOR_DHT22_TEMP, S_TEMP);
-MyMessage msgMCP9700Temp(SENSOR_MCP9700_TEMP, S_TEMP);
-DHT dht22;
+const char *sketch_name = "devduino-temp-hum-rf24";
+const char *sketch_version = "0.2";
 
 /**********************************/
 /********* IMPLEMENTATION *********/
 /**********************************/
-void setup() {
-  node.begin(NULL /* inc msg callback */, NODE_ID);
-  dht22.setup(DHT22_pin);
+void setup()
+{
+  #ifdef WITH_SENSOR_DHT22
+  dht.begin();
+  #endif
+  #ifdef WITH_SENSOR_HTU21
+  htu.begin();
+  #endif
+}
 
-  node.sendSketchInfo("devduino-temp-hum-sensor", "0.1");
+void presentation()
+{
+  sendSketchInfo(sketch_name, sketch_version, false);
 
-  node.present(SENSOR_DHT22_HUM, S_HUM);
-  node.present(SENSOR_DHT22_TEMP, S_TEMP);
-  node.present(SENSOR_MCP9700_TEMP, S_TEMP);
+  #ifdef WITH_SENSOR_DHT22
+  present(SENSOR_DHT22_HUM, S_HUM, "DHT22 Humidity");
+  present(SENSOR_DHT22_TEMP, S_TEMP, "DHT22 Temperature");
+  #endif
+  #ifdef WITH_SENSOR_MCP9700
+  present(SENSOR_MCP9700_TEMP, S_TEMP, "MCP9700 Temperature");
+  #endif
+  #ifdef WITH_SENSOR_HTU21
+  present(SENSOR_HTU21_HUM, S_HUM, "HTU21 Humidity");
+  present(SENSOR_HTU21_TEMP, S_TEMP, "HTU21 Temperature");
+  #endif
 }
 
 void loop() {
-  node.sendBatteryLevel(getVccLevel());
+  sendBatteryLevel(getVccLevel());
 
-  node.send(msgDHT22Hum.set(readDHT22Hum(), 2));
-  node.send(msgDHT22Temp.set(readDHT22Temp(), 2));
-  node.send(msgMCP9700Temp.set(readMCP9700Temp(), 2));
+  #ifdef WITH_SENSOR_DHT22
+  send(msgDHT22Hum.set(readDhtHum(), 2));
+  send(msgDHT22Temp.set(readDhtTemp(), 2));
+  #endif
+  #ifdef WITH_SENSOR_MCP9700
+  send(msgMCP9700Temp.set(readMCP9700Temp(), 2));
+  #endif
+  #ifdef WITH_SENSOR_HTU21
+  send(msgHTU21Hum.set(readHTU21Hum(), 2));
+  send(msgHTU21Temp.set(readHTU21Temp(), 2));
+  #endif
 
-  node.sleep(SLEEP_TIME);
+  smartSleep(SLEEP_TIME);
 }
 
+#ifdef WITH_SENSOR_DHT22
 /**
- * Read the humidity from DHT22
+ * Read the humidity from DHTXX
  */
-float readDHT22Hum() {
-  float hum = dht22.getHumidity();
-  #if DEBUG
+float readDhtHum() {
+  float hum = dht.readHumidity();
+  #ifdef MY_DEBUG
   Serial.print(F("Read Hum from DHT22 = "));
   Serial.println(hum);
   #endif
@@ -108,17 +200,19 @@ float readDHT22Hum() {
 }
 
 /**
- * Read the temperature from DHT22
+ * Read the temperature from DHTXX
  */
-float readDHT22Temp() {
-  float temp = dht22.getTemperature();
-  #if DEBUG
+float readDhtTemp() {
+  float temp = dht.readTemperature();
+  #ifdef MY_DEBUG
   Serial.print(F("Read Temp from DHT22 = "));
   Serial.println(temp);
   #endif
   return temp;
 }
+#endif /* WITH_SENSOR_DHT22 */
 
+#ifdef WITH_SENSOR_MCP9700
 /**
  * Read the temperature from MCP9700
  */
@@ -126,21 +220,48 @@ float readMCP9700Temp() {
   float temp = analogRead(MCP9700_pin)*3.3/1024.0;
   temp = temp - 0.5;
   temp = temp / 0.01;
-  #if DEBUG
+  #ifdef MY_DEBUG
   Serial.print(F("Read Temp from MCP9700 = "));
   Serial.println(temp);
   #endif
   return temp;
 }
+#endif /* WITH_SENSOR_MCP9700 */
+
+#ifdef WITH_SENSOR_HTU21
+/**
+ * Read the humidity from HTU21
+ */
+float readHTU21Hum() {
+  float hum = htu.readHumidity();
+  #ifdef MY_DEBUG
+  Serial.print(F("Read Hum from HTU21 = "));
+  Serial.println(hum);
+  #endif
+  return hum;
+}
+
+/**
+ * Read the temperature from HTU21
+ */
+float readHTU21Temp() {
+  float temp = htu.readTemperature();
+  #ifdef MY_DEBUG
+  Serial.print(F("Read Temp from HTU21 = "));
+  Serial.println(temp);
+  #endif
+  return temp;
+}
+#endif /* WITH_SENSOR_HTU21 */
 
 /**
  * Get the percentage of power in the battery
  */
 uint8_t getVccLevel() {
-  static const float full_battery_v = 3169.0;
-  float level = readVcc() / full_battery_v;
-  uint8_t percent = level * 100;
-  #if DEBUG
+  long vcc = readVcc();
+  vcc -= 1900;    // 1.9 V will be the lowest operational voltage
+  uint8_t percent = vcc / 14.0;
+  #ifdef MY_DEBUG
   Serial.print(F("Battery state = "));
   Serial.println(percent);
   #endif
@@ -176,7 +297,7 @@ uint16_t readVcc() {
 
   result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
   
-  #if DEBUG
+  #ifdef MY_DEBUG
   Serial.print(F("Read Vcc = "));
   Serial.println(result);
   #endif
